09c817faebee4c0fe84538fba183ce67bd4fa497
hermes3306 <joonho.park@hotmail.com>
2018-12-18 15:26:11 +0900

    v1

--- Mail/OB.sh
+++ Mail/OB.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
+
+#!/bin/bash
+
+
+echo "START sending email... " >> /tmp/OB.log
+echo `date` >> /tmp/OB.log
+echo "" >> /tmp/OB.log
+echo "" >> /tmp/OB.log
+
 php /root/OB/Mail/OB.php
 #php /home/pi/code/OB/Mail/OB.php
+
+echo `date` >> /tmp/OB.log
+echo "END" >> /tmp/OB.log
+echo "" >> /tmp/OB.log
+echo "" >> /tmp/OB.log
+echo "" >> /tmp/OB.log
+
--- sql/cr_obemail.sh
+++ sql/cr_obemail.sh
@@ -1,1 +1,11 @@
-mysql -uroot -pWaffleB2BProject waffle <  cr_obemail.sql
+echo "START generating email receipients" >> /tmp/OB.log
+echo `date` >> /tmp/OB.log
+
+mysql -v -uroot -pWaffleB2BProject waffle <  /root/OB/sql/cr_obemail.sql >> /tmp/OB.log
+#mysql -v -uwaffle -pwaffle waffle <  /home/pi/code/OB/sql/ob_email.sql >> /tmp/OB.log
+
+echo `date` >> /tmp/OB.log
+echo "END" >> /tmp/OB.log
+echo "" >> /tmp/OB.log
+echo "" >> /tmp/OB.log
+echo "" >> /tmp/OB.log
